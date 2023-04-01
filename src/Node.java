9c2476f54cc49b107f3f5ac86f960439f0ec2f20
Artem Walkup <godzilla@godzilla.lan>
2023-04-01 19:41:44 +0300

    added getters and setters

--- src/Node.java
+++ src/Node.java
@@ -9,8 +9,30 @@ public class Node {
 
    Node (String n, Node d, Node r) {
       // TODO!!! Your constructor here
+      setName(n);
+      setFirstChild(d);
+      setNextSibling(r);
    }
-   
+
+   public Node getFirstChild(){return this.firstChild;}
+
+   public Node getNextSibling(){return this.nextSibling;}
+
+   public String getName(){return this.name;}
+
+   public void setName(String n){
+      this.name = n;
+   }
+
+   public void setNextSibling(Node p){
+      this.nextSibling = p;
+   }
+
+   public void setFirstChild(Node n){
+      this.firstChild = n;
+   }
+
+
    public static Node parsePostfix (String s) {
       return null;  // TODO!!! return the root
    }
